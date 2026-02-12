ab31852d80ad6e40a3413b78c553e5f96c39c02b
AlexBuenoL <abuenoleon@gmail.com>
2026-02-12 19:25:22 +0100

    implemented data collator for completion only class

---
+++ src/collators.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import DataCollatorForLanguageModeling
+
+class CTICompletionColator(DataCollatorForLanguageModeling):
+    def __init__(self, response_template, tokenizer, *args, **kwargs):
+        super().__init__(tokenizer=tokenizer, mlm=False, *args, **kwargs)
+        self.response_template = response_template
+        self.tokenizer = tokenizer
+
+    def torch_call(self, examples):
+        batch = super().torch_call(examples)
+
+        # Identify token IDs for "### Response:"
+        response_token_ids = self.tokenizer.encode(self.response_template, add_special_tokens=False)
+
+        for i in range(len(batch["labels"])):
+            input_ids = batch["input_ids"][i].to_list()
+
+            # Find the slice where the actual answer starts
+            response_start_idx = -1
+            for idx in range(len(input_ids) - len(response_token_ids) + 1):
+                if input_ids[idx : idx + len(response_token_ids)] == response_token_ids:
+                    response_start_idx = idx + len(response_token_ids)
+                    break
+            
+            # Apply the masking (-100) to the instruction part
+            if response_start_idx != -1:
+                batch["labels"][i, :response_start_idx] = -100
+        
+        return batch
--- src/train.py
+++ src/train.py
@@ -1,17 +1,48 @@
-from transformers import AutoTokenizer, DataCollatorForCompletionOnlyLM
-from config import MODEL_NAME
+from datasets import load_from_disk
+from transformers import AutoTokenizer
+from torch.utils.data import DataLoader
+from config import MODEL_NAME, PREPROCESSED_DATA_PATH
+from collators import CTICompletionColator
 
 
-if __name__ == '__main__':
+def train():
+    # Load preprocessed dataset from disk
+    ds = load_from_disk(PREPROCESSED_DATA_PATH)
+
+    # Load tokenizer    
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
     tokenizer.pad_token = tokenizer.eos_token
 
-    # Ensure the model only learns to generate the JSON. During training:
+    # Data collator: Ensure the model only learns to generate the JSON. During training:
     # - Before the template, it replaces every tokenID of Instruction and Input with -100 
     #   --> Model weights are not updated
     # - After the template, the model uses the actual tokenIDs
     #   --> Model is trained only on predicting the JSON response
-    data_collator = DataCollatorForCompletionOnlyLM(
+    data_collator = CTICompletionColator(
         response_template="### Response:",
         tokenizer=tokenizer
     )
+
+    # Build Data Loaders
+    train_loader = DataLoader(
+        ds["train"],
+        batch_size=16,
+        shuffle=True,
+        collate_fn=data_collator
+    )
+    val_loader = DataLoader(
+        ds["validation"],
+        batch_size=16,
+        shuffle=False,
+        collate_fn=data_collator
+    )
+    test_loader = DataLoader(
+        ds["test"],
+        batch_size=16,
+        shuffle=False,
+        collate_fn=data_collator
+    )
+
+
+if __name__ == '__main__':
+    train()
